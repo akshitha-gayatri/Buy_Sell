8c5ec22fdd8ce02be5cdfeb5f3bbfc6ece9b5159
akshitha-gayatri <142877999+akshitha-gayatri@users.noreply.github.com>
2025-05-22 13:17:49 +0530

    Delete frontend directory

--- frontend/src/AuthCheck.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const AuthCheck = ({ children }) => {
-  const isAuthenticated = !!localStorage.getItem('token');
-  return isAuthenticated ? children : <Navigate to="/login" replace />;
-};
-
-export default AuthCheck;
